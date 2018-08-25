09f524bda5258a1cd5eaea9c664e6942915c659f
arrow2851 <omar.h.mohiuddin@gmail.com>
2018-08-25 19:52:49 +0530

    Added mptt-admin
    mptt-admin helps with better visualization for tree objects. In this case. comment chains, posts, etc.

--- VnewsDRF/sub/admin.py
+++ VnewsDRF/sub/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
+from django_mptt_admin.admin import DjangoMpttAdmin
 
 from sub.models import Post, Comment, Class, News, Clubs
+class CommentAdmin(DjangoMpttAdmin):
+    pass
 
-admin.site.register(Comment)
+
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(News)
 admin.site.register(Clubs)
 admin.site.register(Class)
